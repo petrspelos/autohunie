49cb032ca7996485a8e23ebb0bb89027d724b557
petrspelos <petrspelos@users.noreply.github.com>
2023-04-06 15:55:34 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+![](docs/logo.png)
+
+An automation for the game Hunie Pop 2
+
+## Made with
+
+- .NET 8
+
+> :warning: targets Windows 10
---
+++ src/AutoHunie.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+Console.Clear();
+var logoMarkup = File.ReadAllText("logo.lgo");
+
+
+
+var codeRegex = new Regex(@"\[(\d+)\]", RegexOptions.Compiled);
+
+for (var i = 0; i < logoMarkup.Length; i++) {    
+    if (logoMarkup[i] == '[') {
+        var match = codeRegex.Match(logoMarkup.Substring(i));
+        var capture = match.Groups.Values.ElementAtOrDefault(1);
+
+        if (capture is null) {
+            continue;
+        }
+
+        Console.ForegroundColor = (ConsoleColor)int.Parse(capture.Value);
+        i += capture.Value.Length + 2;
+    }
+    
+    Console.Write(logoMarkup[i]);
+}
+
+
+
+foreach (ConsoleColor c in (ConsoleColor[]) Enum.GetValues(typeof(ConsoleColor)))
+{
+    Console.ForegroundColor = c;
+    Console.WriteLine($"[{(int)c}] Hello, HuniePop2! [{c}]");
+}
